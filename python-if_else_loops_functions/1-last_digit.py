8cf1b967efb9a8d32041a2a2aeb99758988beee8
CanonicCake <n.a.ellis99@gmail.com>
2023-09-12 20:29:09 -0700

    reverse modulo

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastn = int(repr(number)[-1])
-if lastn > 5:
-    print(f"Last digit of {number} is {lastn} and is greater than 5")
+if number > 0:
+    lastn = number % 10
+elif number < 0:
+    lastn = (((number * -1) % 10) * -1)
+if lastn < 6 and lastn != 0:
+    print(f"Last digit of {number} is {lastn} and is less than 6 and not 0")
 elif lastn == 0:
     print(f"Last digit of {number} is {lastn} and is 0")
-else:
-    print(f"Last digit of {number} is {lastn} and is less than 6 and not 0")
+elif lastn > 5:
+    print(f"Last digit of {number} is {lastn} and is greater than 5")
