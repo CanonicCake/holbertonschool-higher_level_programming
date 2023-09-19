1a60c07a02fc8ff64cfc18cdb813daca56dbbbf6
CanonicCake <n.a.ellis99@gmail.com>
2023-09-19 00:31:02 -0700

    had to take out the extra -1 making the negatives, and added a print to get all numbers

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,5 +3,6 @@ def print_last_digit(number):
     if number >= 0:
         lastn = number % 10
     elif number < 0:
-        lastn = (((number * -1) % 10) * -1)
+        lastn = (number * -1) % 10
+    print(lastn, end="")
     return lastn
