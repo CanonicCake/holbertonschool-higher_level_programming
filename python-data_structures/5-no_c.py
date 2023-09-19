17610cf891bd4fee733ff34739037ee814fe9869
CanonicCake <n.a.ellis99@gmail.com>
2023-09-19 11:54:47 -0700

    syntax error

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/bin/usr/python3
 def no_c(my_string):
     """Remove all c and C from list"""
-    str = ""
+    str = ''
     for i in my_string:
         if i != 'c' and i != 'C':
             str += i
