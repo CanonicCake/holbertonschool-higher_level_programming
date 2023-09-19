e375b9958c5137ce01d80ee40c4c0da89c34d7ad
CanonicCake <n.a.ellis99@gmail.com>
2023-09-19 11:34:00 -0700

    elemental replacement, and check in place

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     """ Print a replacement of an element"""
-    if idx >= 0 or idx < (len(my_list)):
+
+    if idx >= 0 or idx < len(my_list):
         my_list[idx] = element
-    return my_list
+    return (my_list)
