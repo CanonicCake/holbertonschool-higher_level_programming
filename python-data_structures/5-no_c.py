7b6b96d0799ffeecd4dac9149baa535de31484a8
CanonicCake <n.a.ellis99@gmail.com>
2023-09-19 12:05:07 -0700

    .join in return

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/bin/usr/python3
 def no_c(my_string):
     """Remove all c and C from list"""
-    new_str = ''
+    new_str = ""
     for i in my_string:
         if i != 'C' and i != 'c':
             new_str += i
-    return new_str
+    return ("".join(new_str))
