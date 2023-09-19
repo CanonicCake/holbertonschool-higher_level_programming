245f523bd2725336093e43f1763c3959e84a83b2
CanonicCake <n.a.ellis99@gmail.com>
2023-09-19 11:58:54 -0700

    tried changing to new_str

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/bin/usr/python3
 def no_c(my_string):
     """Remove all c and C from list"""
-    str = ''
+    new_str = ''
     for i in my_string:
         if i != 'C' and i != 'c':
-            str += i
-    return str
+            new_str += i
+    return new_str
