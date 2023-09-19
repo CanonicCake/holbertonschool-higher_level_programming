bfa44658a83fe3743ad7e65875e7af5a45b8f66e
CanonicCake <n.a.ellis99@gmail.com>
2023-09-19 02:45:38 -0700

    syntax error

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,6 +3,7 @@ if __name__ == "__main__":
     """ prints the numbers on an argument list
     will grab from the given line and print back
     under an argument"""
+
 import sys
 av = sys.argv
 lowav = len(av) - 1
@@ -16,4 +17,4 @@ elif lowav == 1:
     for i in range(1, lowav + 1):
         print("{:d}: {}".format(i, av[i]))
 elif lowav == 0:
-    print(lowav, 'arguments:')
+    print(lowav, 'arguments.')
