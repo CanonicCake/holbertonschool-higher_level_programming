cad39e12a68f9993b6f2bb25d1ac9a526715d257
CanonicCake <n.a.ellis99@gmail.com>
2023-09-19 14:35:43 -0700

    testing on 3 square v2

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -12,7 +12,7 @@ class Square:
         Args:
             size (int) = The size of the class Square.
         """
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
