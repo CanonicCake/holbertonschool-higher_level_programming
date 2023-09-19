138b49199d8883a007191d2ad9606c6adb12425e
CanonicCake <n.a.ellis99@gmail.com>
2023-09-19 14:13:32 -0700

    type class

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,12 +10,12 @@ class Square:
     def __init__(self, size=0):
         """__init__
 
-        The __init__ method initalizes a new Square.
+        The __init__ method of class Square.
 
         Args:
             size (int): The size of the new square.
         """
-        if not isinstance(size, int):
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise TypeError("size must be >= 0")
