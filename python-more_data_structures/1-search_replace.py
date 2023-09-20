c35c3e4f3e8c9cc872aa15a248fbe9b77c6f6f0c
CanonicCake <n.a.ellis99@gmail.com>
2023-09-20 15:19:20 -0700

    i needed to be set before it could itterate

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    i = 0
+    while i < len(my_list):
+        if my_list[i] == search:
+            my_list.append(replace)
+        else:
+            new_list.append(my_list[i])
+        i += 1
+    return new_list
