f1163976e6b3411b3ccb49f1e5661543c872525c
CanonicCake <n.a.ellis99@gmail.com>
2023-09-20 16:53:53 -0700

    safe print integer was in the wrong file

--- python-exceptions/6-raise_exception.py
+++ python-exceptions/6-raise_exception.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    try:
-        print("{:d}".format(value))
-        return True
-    except (TypeError, ValueError):
-        return False
