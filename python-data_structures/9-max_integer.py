2c957ce35e58e5403fede9e39f2f937ca85b8d90
CanonicCake <n.a.ellis99@gmail.com>
2023-09-20 10:30:14 -0700

    none return incorrect

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if not my_list:
-        return (0, None)
+        return None
     my_l = min(my_list)
     for i in my_list:
         if my_l < i:
