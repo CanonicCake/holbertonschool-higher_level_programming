498ca215df871db2a4b42a6e1493c45a07730fa1
CanonicCake <n.a.ellis99@gmail.com>
2023-09-19 22:25:44 -0700

    updating 2-arv

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
+from sys import argv as aug
+
 if __name__ == "__main__":
-    """ prints the numbers on an argument list
-    will grab from the given line and print back
-    under an argument"""
-
-import sys
-
-aug = sys.argv
-count = len(aug) - 1
-
-if count == 0:
-    print('0 arguments:')
-else:
-    print("{} arguments:".format(count))
-for i in range(count):
-    print("{}: {}".format(i + 1, aug[i + 1]))
+    if len(aug) - 1 == 0:
+        print('0 arguments:')
+    else:
+        if len(aug) - 1 == 1:
+            print("{} arguments:".format(len(aug) - 1))
+        else:
+            print("{} arguments:".format(len(aug) - 1))
+        for i in range(1, len(aug)):
+            print("{}: {}".format(i, aug[i]))
