c2250c937f1a7fe7d154357208777d07d5da2f4c
CanonicCake <n.a.ellis99@gmail.com>
2023-09-20 16:51:28 -0700

    safe print integer

--- python-exceptions/6-raise_exception.py
+++ python-exceptions/6-raise_exception.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        return False
