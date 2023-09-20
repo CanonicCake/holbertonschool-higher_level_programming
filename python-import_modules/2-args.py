08e1a52c67ea96f44c422aa5ba2b0d07150579c4
CanonicCake <n.a.ellis99@gmail.com>
2023-09-19 22:26:50 -0700

    typo

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         print('0 arguments:')
     else:
         if len(aug) - 1 == 1:
-            print("{} arguments:".format(len(aug) - 1))
+            print("{} argument:".format(len(aug) - 1))
         else:
             print("{} arguments:".format(len(aug) - 1))
         for i in range(1, len(aug)):
