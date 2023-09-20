c05295774328acb2cf15e440eebc972802fecc92
CanonicCake <n.a.ellis99@gmail.com>
2023-09-19 22:27:41 -0700

    another typo

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,7 @@ from sys import argv as aug
 
 if __name__ == "__main__":
     if len(aug) - 1 == 0:
-        print('0 arguments:')
+        print('0 arguments.')
     else:
         if len(aug) - 1 == 1:
             print("{} argument:".format(len(aug) - 1))
