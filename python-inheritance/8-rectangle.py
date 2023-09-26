2fd428f71d4475ebc0c475f5dc70f594e923e839
CanonicCake <n.a.ellis99@gmail.com>
2023-09-26 14:49:23 -0700

    test 1

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,17 +5,6 @@
 class BaseGeometry:
     """Defines a base class geometry"""
 
-    def area(self):
-        """Defines the area of geometry"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Integer Validation that raises value"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
     def __init__(self, width, height):
         """Initilizes self width and height"""
         if self.integer_validator("width", width):
