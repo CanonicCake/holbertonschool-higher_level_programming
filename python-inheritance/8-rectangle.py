d942802bffe0bdcac7f95dd9de53c13c110901eb
CanonicCake <n.a.ellis99@gmail.com>
2023-09-26 14:52:59 -0700

    needed to import not rewrite 7

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Public instance of area and self for base geometry"""
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class BaseGeometry:
-    """Defines a base class geometry"""
+    """Defines a class Rectangle"""
 
     def __init__(self, width, height):
-        """Initilizes self width and height"""
         if self.integer_validator("width", width):
             self.__width = width
         if self.integer_validator("height", height):
