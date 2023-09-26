1a682320f32d42ba296720dce646d1fdb9fe61dd
CanonicCake <n.a.ellis99@gmail.com>
2023-09-26 14:54:17 -0700

    forgot to change class

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,7 +4,7 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class BaseGeometry:
+class Rectangle(BaseGeometry):
     """Defines a class Rectangle"""
 
     def __init__(self, width, height):
