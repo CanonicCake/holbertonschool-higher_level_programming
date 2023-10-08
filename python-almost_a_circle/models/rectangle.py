af9f7c31527d19d9a02eda54e26f3afa59919201
CanonicCake <n.a.ellis99@gmail.com>
2023-10-08 12:11:24 -0700

    rectangle class getting and setting

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -12,3 +12,45 @@ class Rectangle:
         self.x = x
         self.y = y
         super().__init__(id)
+
+    @property
+    def height(self):
+        """Property of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """Setting the height"""
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = height
+
+    @property
+    def x(self):
+        """Properties of x"""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """Sets x"""
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = x
+
+    @property
+    def y(self):
+        """Properties of y"""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """Setting y"""
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = y
