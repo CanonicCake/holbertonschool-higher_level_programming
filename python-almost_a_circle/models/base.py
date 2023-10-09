e07874ceff2f1576141115c0e003cf7a58dde7ff
CanonicCake <n.a.ellis99@gmail.com>
2023-10-08 20:33:21 -0700

    saving to json now

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -14,3 +14,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @classmethod
+    def to_json_string(list_dictionaries):
+        """json string to dict by serialization"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
