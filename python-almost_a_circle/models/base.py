a8b558749247587c7d89d350a21a36f74975c293
CanonicCake <n.a.ellis99@gmail.com>
2023-10-08 20:39:04 -0700

    quick syntax fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,7 +17,7 @@ class Base:
 
     @classmethod
     def to_json_string(list_dictionaries):
-        """JSON string to dict by serialization"""
+        """JSON string by serialization"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
