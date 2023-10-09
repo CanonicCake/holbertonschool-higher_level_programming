8e20759489a7ebce226c28c7773e2ef25c1f3192
CanonicCake <n.a.ellis99@gmail.com>
2023-10-09 12:43:31 -0700

    save to file jason

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,15 +18,17 @@ class Base:
     @classmethod
     def to_json_string(list_dictionaries):
         """JSON string by serialization"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
         """Wrties json string to file"""
-        if list_objs is None:
-            list_objs = []
         filename = cls.__name__ + ".json"
+        json_data = []
+        if list_objs is not None:
+            for obj in list_objs:
+                json_data.append(obj.to_dictionary())
         with open(filename, "w") as file:
-            file.write(cls.to_json_string(json_))
+            file.write(cls.to_json_string(json_data))
