6983ee847fe0de2075decfec05114a8a5dae84fd
CanonicCake <n.a.ellis99@gmail.com>
2023-10-24 15:43:14 -0700

    test 1

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all cities from hbtn_0d_usa
 SELECT cites.id, cities.name, states.name
-FROM cities
-JOIN states ON cites.state_id = states_id
+FROM cities, states
+WHERE cites.state_id = states_id
 ORDER BY cities.id;
