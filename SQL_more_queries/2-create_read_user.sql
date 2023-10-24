2c686a1f7d4dbdcc1f62ee5d1ac856f0d6d369d7
CanonicCake <n.a.ellis99@gmail.com>
2023-10-24 14:47:24 -0700

    forgot to push, making user_0d_2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Creates user_0d_2 and sets pasword and privileges
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
