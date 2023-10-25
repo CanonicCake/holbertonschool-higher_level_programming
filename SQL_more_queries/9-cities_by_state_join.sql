fc2b3142b8e8b8563423727ef140c7184022a81a
CanonicCake <n.a.ellis99@gmail.com>
2023-10-24 19:09:50 -0700

    trying left join

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all cities from hbtn_0d_usa
 SELECT cites.id, cities.name, states.name
 FROM cities
-JOIN states ON cites.state_id = states.id
-ORDER BY cities.id;
+LEFT JOIN states ON cites.state_id = states.id
+ORDER BY id ASC;
