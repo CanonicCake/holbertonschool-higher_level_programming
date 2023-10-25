ba5a7b7e90988950312f37ee83946b31ab5f3909
CanonicCake <n.a.ellis99@gmail.com>
2023-10-24 21:41:25 -0700

    second inner join added

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,7 @@
 -- List database hbtn_0d_tvshows
 SELECT tv_genres.name
-FROM tv_show_genres
+FROM tv_shows
+INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
