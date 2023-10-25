aef99dd50310c1806bde4e02377df7afcec8c679
CanonicCake <n.a.ellis99@gmail.com>
2023-10-24 21:28:01 -0700

    group by linked atributes

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- List all genres under hbtn_0d_tvshows that are linked
+SELECT tv_genres.name AS genre, COUNT(show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
