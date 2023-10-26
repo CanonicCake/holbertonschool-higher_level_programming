ee8eb4dace79f3c950743ac7daa5b6bff4644389
CanonicCake <n.a.ellis99@gmail.com>
2023-10-26 14:28:27 -0700

    delete and replace

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Delete all states objects with a new contaner for the letter a"""
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2],
+                                   argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.contains('a')).all()
+    for state in states:
+        session.delete(state)
+    session.commit()
