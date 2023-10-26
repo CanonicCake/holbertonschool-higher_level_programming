3d6ed31dfa0ecedb6fc90885bba60a90beeaa009
CanonicCake <n.a.ellis99@gmail.com>
2023-10-26 12:49:17 -0700

    typo

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     cursor = db.cursor()
     cursor.execute('''SELECT cities.id, cities.name, states.name
                    FROM cities
-                   JOIN state ON cities.state_id = state.id
+                   JOIN states ON cities.state_id = states.id
                    ORDER BY cities.id ASC;
                    ''')
     cities = cursor.fetchall()
