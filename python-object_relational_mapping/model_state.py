70f760179ae94e7648f713b982392328e912ef66
CanonicCake <n.a.ellis99@gmail.com>
2023-10-26 13:04:12 -0700

    incorrect import, import fixed

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Contains Class and Definition of staate"""
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declaration import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
