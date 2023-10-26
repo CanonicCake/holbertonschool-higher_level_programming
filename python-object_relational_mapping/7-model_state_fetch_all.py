e63b2efcd56e2facef7f964f19a52af92369e091
CanonicCake <n.a.ellis99@gmail.com>
2023-10-26 13:14:25 -0700

    engine and session

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""List all State objects from the database hbtn_0e_0_usa under an engine"""
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    engine = create_engine(f'mysql+mysqldb://{argv[1]}:{argv[2]}\
+                           @localhost:3306/{argv[3]}', pool_pre_ping=True)
+    Base.metadate.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        print(f'{state.id}: {state.name}')
