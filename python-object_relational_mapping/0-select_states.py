a18d29832c202c33197748a7b80982422e7c9462
CanonicCake <n.a.ellis99@gmail.com>
2023-10-26 12:20:17 -0700

    typo

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Module documentation: Lists all states from the database hbtn_0d_0_usa"""
+"""Module documentation: Lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 from sys import argv
 
 
 if __name__ == '__main__':
-    """List all states from database"""
+    """List all states from database hbtn_0e_0_usa"""
 
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute('SELECT * FROM states')
     states = cursor.fetchall()
-    for states in states:
+    for state in states:
         print(state)
