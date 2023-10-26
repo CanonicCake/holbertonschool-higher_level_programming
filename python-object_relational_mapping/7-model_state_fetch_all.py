7290225bd3e0fdbbfdb1757049c1a36eda3ccb51
CanonicCake <n.a.ellis99@gmail.com>
2023-10-26 13:20:32 -0700

    session close

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,3 +13,4 @@ if __name__ == '__main__':
     session = Session()
     for state in session.query(State).order_by(State.id).all():
         print(f'{state.id}: {state.name}')
+    session.close()
